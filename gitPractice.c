0ad5236f1c1731bf5ba0b1ecc3997a71e1ac13be
Nelsonwin <nelsonjamb2004@gmail.com>
2021-09-02 10:30:42 +0100

    swaps the values of two numbers

---
+++ gitPractice.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int x = 28;
+    int y = 50;
+
+    printf("x is %d\n", x);
+    printf("y is %d\n", y);
+    
+    //swapping...
+    int temp = x;
+    x = y;
+    y = temp;
+
+    printf("x is now %d\n", x);
+    printf("y is now %d\n", y);
+
+
+    return 0;
+}
